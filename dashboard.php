7258b3ca233ff2e92c2b92a7dcd13756e6ca426f
Tarek <tfzein@gmail.com>
2020-05-06 20:21:28 +0200

    Modified page titles for many pages. Removed the <meta viewport for mobile responsive design. forced desktop mode all the time. If project specifications change to require mobile version, this can be added later on request.

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,7 @@ else
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+<!--    <meta name="viewport" content="width=device-width,initial-scale=1.0">-->
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="javascript/scripts.js"></script>
@@ -27,7 +27,7 @@ else
     <script src="https://kit.fontawesome.com/dedb547a55.js" crossorigin="anonymous"></script>
     <!--    Google Font-->
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap" rel="stylesheet">
-    <title>SomeBlog Home Page</title>
+    <title><?php echo $user['username']?>'s dashboard</title>
 
 </head>
 <body>
